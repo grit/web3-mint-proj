8f0378cff90f4c1c14f2f67332f96e3ca71e30f2
proxyGrit <dev@proxygrit.com>
2022-03-06 22:22:33 -0800

    Configure base project directory structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web3-mint-proj
+
 A full Web3 minting website using React, Hardhat, Ethers, and Chakra
---
+++ contracts/ProxyPunks.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+'import @openzeppelin/contracts/token/ERC721/ERC721.sol';
+'import @openzeppelin/contracts/access/Ownable.sol';
+
+contract ProxyPunks is ERC721, Ownable {
+
+}
